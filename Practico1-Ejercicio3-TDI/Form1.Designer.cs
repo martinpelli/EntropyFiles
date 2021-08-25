2ca6a563f5f8afcf02f40bbc27c9590c6743ad0d
Pelli <friedstudios@outlook.com>
2021-08-25 12:33:53 -0300

    interface created

--- Practico1-Ejercicio3-TDI/Form1.Designer.cs
+++ Practico1-Ejercicio3-TDI/Form1.Designer.cs
@@ -29,13 +29,65 @@ namespace Practico1_Ejercicio3_TDI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.loadFile1Button = new System.Windows.Forms.Button();
+            this.loadFile2Button = new System.Windows.Forms.Button();
+            this.tittleLabel = new System.Windows.Forms.Label();
+            this.openFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // loadFile1Button
+            // 
+            this.loadFile1Button.Location = new System.Drawing.Point(52, 113);
+            this.loadFile1Button.Name = "loadFile1Button";
+            this.loadFile1Button.Size = new System.Drawing.Size(142, 51);
+            this.loadFile1Button.TabIndex = 0;
+            this.loadFile1Button.Text = "Cargar Primer Archivo";
+            this.loadFile1Button.UseVisualStyleBackColor = true;
+            // 
+            // loadFile2Button
+            // 
+            this.loadFile2Button.Location = new System.Drawing.Point(276, 113);
+            this.loadFile2Button.Name = "loadFile2Button";
+            this.loadFile2Button.Size = new System.Drawing.Size(156, 51);
+            this.loadFile2Button.TabIndex = 1;
+            this.loadFile2Button.Text = "Cargar Segundo Archivo";
+            this.loadFile2Button.UseVisualStyleBackColor = true;
+            // 
+            // tittleLabel
+            // 
+            this.tittleLabel.AutoSize = true;
+            this.tittleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tittleLabel.Location = new System.Drawing.Point(81, 38);
+            this.tittleLabel.Name = "tittleLabel";
+            this.tittleLabel.Size = new System.Drawing.Size(328, 31);
+            this.tittleLabel.TabIndex = 2;
+            this.tittleLabel.Text = "Comparador De Entropías";
+            // 
+            // openFile
+            // 
+            this.openFile.FileName = "openFile";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(483, 218);
+            this.Controls.Add(this.tittleLabel);
+            this.Controls.Add(this.loadFile2Button);
+            this.Controls.Add(this.loadFile1Button);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button loadFile1Button;
+        private System.Windows.Forms.Button loadFile2Button;
+        private System.Windows.Forms.Label tittleLabel;
+        private System.Windows.Forms.OpenFileDialog openFile;
     }
 }
 
